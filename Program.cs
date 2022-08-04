0b03341789a0ea43cb2257791fdb4709b9b876a3
BernardoMoschen <bernardomoschen.dev@gmail.com>
2022-08-04 13:48:00 -0300

    wip: first var

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@ namespace MyFirstApp
   class Program {
     static void Main(string[] args) {
       Console.WriteLine("Hello World!");
+
+      int idade;
+      idade = 27;
+      Console.WriteLine(idade);
     }
   }
 }
