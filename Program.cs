dd029431b6a29bf7f6eff2d40c32d8ffbf9d8a1f
BernardoMoschen <bernardomoschen.dev@gmail.com>
2022-08-04 15:14:54 -0300

    wip: learning var types

--- Program.cs
+++ Program.cs
@@ -6,14 +6,17 @@ namespace MyFirstApp
     static void Main(string[] args) {
       Console.WriteLine("Hello World!");
 
-
+      //Var type for text
       string name = "Bernardo"; 
 
       Console.Write($"My name is {name},");
+
+      // Number variable type with 32bits
       int age = 21;
 
       Console.WriteLine($" and my age is {age}.");
       
+      //Double precision point number
       double height = 1.70; 
       Console.WriteLine($"I'm {height} tall.");
       int heightToInteger = (int)height;
@@ -24,11 +27,13 @@ namespace MyFirstApp
       doubleTypeTest = 7.0 / 5.0;
       Console.WriteLine($"After using dots it returns a double: {doubleTypeTest}.");
 
-
+      // Number variable type with 64bits
       long x = 2000000000000000000; 
 
+      // Number variable type with 16bits
       short y = 15000;
 
+      //Single precision point number
       float z = 1.62f;
      
     }
