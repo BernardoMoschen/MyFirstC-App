81a970cb143327c903527e607eeb57f117aa5004
BernardoMoschen <bernardomoschen.dev@gmail.com>
2022-08-04 15:12:26 -0300

    wip: learning doubles

--- Program.cs
+++ Program.cs
@@ -7,32 +7,29 @@ namespace MyFirstApp
       Console.WriteLine("Hello World!");
 
 
-      string name;
-      name = "Bernardo"; 
+      string name = "Bernardo"; 
 
       Console.Write($"My name is {name},");
-      int age;
-      age = 21;
+      int age = 21;
 
       Console.WriteLine($" and my age is {age}.");
       
-      double height;
-      height = 1.70; 
+      double height = 1.70; 
       Console.WriteLine($"I'm {height} tall.");
+      int heightToInteger = (int)height;
 
       Console.WriteLine($"Testing the double type.");
-      double doubleTypeTest; 
-      doubleTypeTest = 7 / 5 ;
+      double doubleTypeTest = 7 / 5 ;
       Console.WriteLine($"Without dots it returns a int: {doubleTypeTest}.");
       doubleTypeTest = 7.0 / 5.0;
       Console.WriteLine($"After using dots it returns a double: {doubleTypeTest}.");
 
 
-      long x;
-      x = 2000000000000000000; 
+      long x = 2000000000000000000; 
 
-      short y;
-      y = 15000;
+      short y = 15000;
+
+      float z = 1.62f;
      
     }
   }
