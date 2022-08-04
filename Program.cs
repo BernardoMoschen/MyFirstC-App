2d2970ff0e1c8fe4dd34ed3cb4f70d3c8658ca57
BernardoMoschen <bernardomoschen.dev@gmail.com>
2022-08-04 14:22:31 -0300

    wip: learning doubles

--- Program.cs
+++ Program.cs
@@ -6,17 +6,27 @@ namespace MyFirstApp
     static void Main(string[] args) {
       Console.WriteLine("Hello World!");
 
+
+      string name;
+      name = "Bernardo"; 
+
+      Console.WriteLine($"My name is {name},");
       int age;
       age = 21;
 
-      Console.WriteLine($"Your age is {age}!");
+      Console.Write($" and my age is {age}.");
       
-      double salary;
-      salary = 3000; 
-      Console.WriteLine($"Your salary is {salary}!");
+      double height;
+      height = 1.70; 
+      Console.WriteLine($"I'm {height} tall.");
 
-      
 
+      Console.WriteLine($"Testing the double type.");
+      double doubleTypeTest; 
+      doubleTypeTest = 7 / 5 ;
+      Console.WriteLine($"Without dots it returns a int: {doubleTypeTest}.");
+      doubleTypeTest = 7.0 / 5.0;
+      Console.WriteLine($"After using dots it returns a double/float: {doubleTypeTest}.");
     }
   }
 }
